27e77bb8f9b1e1477ce63877ed18bec47852a203
BorisoffEugene <Borisoff_Eugene@mail.ru>
2026-05-01 14:39:14 +0300

    Созданы рыбы микросервисов

---
+++ transfer/src/main/java/ru/yandex/practicum/transfer/BankAppTransferApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAppTransferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAppTransferApplication.class, args);
+    }
+
+}
