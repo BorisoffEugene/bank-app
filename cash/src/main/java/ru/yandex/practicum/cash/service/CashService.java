3b37fb064d89c8cc6b801e6425be3b1ea53e3e48
BorisoffEugene <Borisoff_Eugene@mail.ru>
2026-05-06 12:33:21 +0300

    Добавлены notifications в cash и transfer

--- cash/src/main/java/ru/yandex/practicum/cash/service/CashService.java
+++ cash/src/main/java/ru/yandex/practicum/cash/service/CashService.java
@@ -2,8 +2,10 @@ package ru.yandex.practicum.cash.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.cash.client.NotificationClient;
 import ru.yandex.practicum.cash.dto.CashRequestDto;
 import ru.yandex.practicum.cash.dto.CashResponseDto;
+import ru.yandex.practicum.cash.dto.NotificationDto;
 import ru.yandex.practicum.cash.mapper.CashMapper;
 import ru.yandex.practicum.cash.model.Cash;
 import ru.yandex.practicum.cash.repository.CashRepository;
@@ -13,8 +15,16 @@ import ru.yandex.practicum.cash.repository.CashRepository;
 public class CashService {
     private final CashRepository repository;
     private final CashMapper mapper;
+    private final NotificationClient notificationClient;
 
     public CashResponseDto save(CashRequestDto dto) {
+        notificationClient.send(new NotificationDto(String.format(
+                "Транзакция %s %s суммы %d руб.",
+                dto.getAction().equals("GET") ? "снятия со счета" : "пополнения на счет",
+                dto.getAccount(),
+                dto.getAmount()
+        )));
+
         Cash cash = mapper.toEntity(dto);
         Cash saved = repository.save(cash);
         return mapper.toDto(saved);
--- transfer/src/main/java/ru/yandex/practicum/transfer/service/TransferService.java
+++ transfer/src/main/java/ru/yandex/practicum/transfer/service/TransferService.java
@@ -2,6 +2,8 @@ package ru.yandex.practicum.transfer.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.transfer.client.NotificationClient;
+import ru.yandex.practicum.transfer.dto.NotificationDto;
 import ru.yandex.practicum.transfer.dto.TransferRequestDto;
 import ru.yandex.practicum.transfer.dto.TransferResponseDto;
 import ru.yandex.practicum.transfer.mapper.TransferMapper;
@@ -13,8 +15,16 @@ import ru.yandex.practicum.transfer.repository.TransferRepository;
 public class TransferService {
     private final TransferRepository repository;
     private final TransferMapper mapper;
+    private final NotificationClient notificationClient;
 
     public TransferResponseDto save(TransferRequestDto dto) {
+        notificationClient.send(new NotificationDto(String.format(
+                "Перевод со счета %s на счет %s суммы %d руб.",
+                dto.getAccountFrom(),
+                dto.getAccountTo(),
+                dto.getAmount()
+        )));
+
         Transfer transfer = mapper.toEntity(dto);
         Transfer saved = repository.save(transfer);
         return mapper.toDto(saved);
