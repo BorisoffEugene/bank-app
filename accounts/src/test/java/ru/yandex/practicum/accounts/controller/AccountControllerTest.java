98f6d91e9a009d3e9abcdd893c5cade051fbe57c
BorisoffEugene <Borisoff_Eugene@mail.ru>
2026-05-11 13:11:32 +0300

    AccountControllerTest Часть 2

--- accounts/src/test/java/ru/yandex/practicum/accounts/controller/AccountControllerTest.java
+++ accounts/src/test/java/ru/yandex/practicum/accounts/controller/AccountControllerTest.java
@@ -9,13 +9,15 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.yandex.practicum.accounts.dto.AccountRequestDto;
 import ru.yandex.practicum.accounts.dto.AccountResponseDto;
+import ru.yandex.practicum.accounts.dto.CashRequestDto;
+import ru.yandex.practicum.accounts.dto.TransferRequestDto;
 import ru.yandex.practicum.accounts.service.AccountService;
 
 import java.time.LocalDate;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -131,5 +133,81 @@ public class AccountControllerTest {
                 .andExpect(jsonPath("$.login").value(LOGIN))
                 .andExpect(jsonPath("$.name").value(NAME))
                 .andExpect(jsonPath("$.birthdate").value(BIRTHDATE.toString()));
+    }
+
+    @Test
+    @DisplayName("Изменение суммы")
+    void testChangeSum_Success() throws Exception {
+        CashRequestDto cashRequest = CashRequestDto.builder()
+                .account(LOGIN)
+                .action("PUT")
+                .amount(100)
+                .build();
+
+        doNothing().when(accountService).changeSum(any(CashRequestDto.class));
+
+        mockMvc.perform(post("/account/change")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(cashRequest))
+                        .with(jwt().jwt(builder -> builder.claim("preferred_username", LOGIN)))
+                )
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Изменение суммы (недостаточно средств)")
+    void testChangeSum_Error() throws Exception {
+        CashRequestDto cashRequest = CashRequestDto.builder()
+                .account(LOGIN)
+                .action("GET")
+                .amount(100)
+                .build();
+
+        doThrow(new IllegalArgumentException("Error")).when(accountService).changeSum(any(CashRequestDto.class));
+
+        mockMvc.perform(post("/account/change")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(cashRequest))
+                        .with(jwt().jwt(builder -> builder.claim("preferred_username", LOGIN)))
+                )
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("Перевод")
+    void testTransfer_Success() throws Exception {
+        TransferRequestDto transferRequest = TransferRequestDto.builder()
+                .accountFrom(LOGIN)
+                .accountTo("petrov")
+                .amount(100)
+                .build();
+
+        doNothing().when(accountService).transfer(any(TransferRequestDto.class));
+
+        mockMvc.perform(post("/account/transfer")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(transferRequest))
+                        .with(jwt().jwt(builder -> builder.claim("preferred_username", LOGIN)))
+                )
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Перевод (недостаточно средств)")
+    void testTransfer_Error() throws Exception {
+        TransferRequestDto transferRequest = TransferRequestDto.builder()
+                .accountFrom(LOGIN)
+                .accountTo("petrov")
+                .amount(100)
+                .build();
+
+        doThrow(new IllegalArgumentException("Error")).when(accountService).transfer(any(TransferRequestDto.class));
+
+        mockMvc.perform(post("/account/transfer")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(transferRequest))
+                        .with(jwt().jwt(builder -> builder.claim("preferred_username", LOGIN)))
+                )
+                .andExpect(status().isNotFound());
     }
 }
