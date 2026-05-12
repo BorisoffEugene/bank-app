dfd75327536013594681f26a0bc779b4b9c0bb2e
BorisoffEugene <Borisoff_Eugene@mail.ru>
2026-05-12 17:13:39 +0300

    контрактное тестирование Notification. Часть 2

--- accounts/src/test/java/ru/yandex/practicum/accounts/client/NotificationClientTest.java
+++ accounts/src/test/java/ru/yandex/practicum/accounts/client/NotificationClientTest.java
@@ -8,17 +8,15 @@ import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties
 import ru.yandex.practicum.accounts.dto.NotificationDto;
 
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = "stubrunner.ids-to-service-ids.notifications=notification-service"
+)
 @AutoConfigureStubRunner(
         ids = {"ru.yandex.practicum:notifications:+:stubs:8085"},
         stubsMode = StubRunnerProperties.StubsMode.LOCAL
 )
 public class NotificationClientTest {
-    static {
-        System.setProperty("spring.cloud.loadbalancer.enabled", "false");
-        System.setProperty("stubrunner.cloud.enabled", "false");
-    }
-
     @Autowired
     private NotificationClient notificationClient;
 
