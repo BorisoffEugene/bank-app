11b2de8bac27e5b658c8342030d84b50fee27ecd
BorisoffEugene <Borisoff_Eugene@mail.ru>
2026-05-24 14:56:43 +0300

    Log. Transfer

--- transfer/src/main/java/ru/yandex/practicum/transfer/service/TransferService.java
+++ transfer/src/main/java/ru/yandex/practicum/transfer/service/TransferService.java
@@ -1,7 +1,9 @@
 package ru.yandex.practicum.transfer.service;
 
 import io.micrometer.core.instrument.Metrics;
+import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.transfer.client.AccountClient;
@@ -13,20 +15,38 @@ import ru.yandex.practicum.transfer.model.Transfer;
 import ru.yandex.practicum.transfer.repository.TransferRepository;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class TransferService {
     private final TransferRepository repository;
     private final TransferMapper mapper;
     private final AccountClient accountClient;
     private final KafkaTemplate<String, NotificationDto> kafkaTemplate;
+    private final Tracer tracer;
+
+    private String getTraceId() {
+        if (tracer.currentTraceContext().context() != null) {
+            return tracer.currentTraceContext().context().traceId();
+        }
+        return "";
+    }
+
+    private String getSpanId() {
+        if (tracer.currentTraceContext().context() != null) {
+            return tracer.currentTraceContext().context().spanId();
+        }
+        return "";
+    }
 
     public TransferResponseDto save(TransferRequestDto dto) {
-        kafkaTemplate.send("notification", new NotificationDto(String.format(
+        String message = String.format(
                 "Перевод со счета %s на счет %s суммы %d руб.",
                 dto.getAccountFrom(),
                 dto.getAccountTo(),
                 dto.getAmount()
-        )));
+        );
+        kafkaTemplate.send("notification", new NotificationDto(message));
+        log.info("{}, {}, {}", message, getTraceId(), getSpanId());
 
         try {
             accountClient.transfer(dto);
