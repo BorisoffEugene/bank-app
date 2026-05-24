bf7ad53498fa6d9d7a136a5070c1dc4dc5b900dd
BorisoffEugene <Borisoff_Eugene@mail.ru>
2026-05-24 14:51:09 +0300

    Log. Cash

--- notifications/src/main/java/ru/yandex/practicum/notifications/service/NotificationListener.java
+++ notifications/src/main/java/ru/yandex/practicum/notifications/service/NotificationListener.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.notifications.service;
 
+import io.micrometer.tracing.Tracer;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -8,9 +10,26 @@ import ru.yandex.practicum.notifications.dto.NotificationDto;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class NotificationListener {
+    private final Tracer tracer;
+
+    private String getTraceId() {
+        if (tracer.currentTraceContext().context() != null) {
+            return tracer.currentTraceContext().context().traceId();
+        }
+        return "";
+    }
+
+    private String getSpanId() {
+        if (tracer.currentTraceContext().context() != null) {
+            return tracer.currentTraceContext().context().spanId();
+        }
+        return "";
+    }
+
     @KafkaListener(topics = "notification")
     public void listenNotifications(ConsumerRecord<String, NotificationDto> record) {
-        log.info(record.value().message());
+        log.info("{}, {}, {}", record.value().message(), getTraceId(), getSpanId());
     }
 }
